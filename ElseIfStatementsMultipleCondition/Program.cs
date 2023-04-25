79143f51f0c3e04470352e232e8e64f2ce57cc98
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-04-25 15:13:45 +0200

    Add project files.

---
+++ ElseIfStatementsMultipleCondition/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElseIfStatementsMultipleCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guests = 3;
+            if (guests >= 4)
+            {
+                Console.WriteLine("Catan");
+            }
+            else
+            if (guests == 1 && guests == 3)
+            {
+                Console.WriteLine("Innovation");
+            }
+            else
+            {
+                Console.WriteLine("Solitaire");
+            }
+        }
+    }
+}
